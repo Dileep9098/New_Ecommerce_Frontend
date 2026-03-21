ff561920c61413e41edac18dacb90331fe6b2b51
Dileep Sahu <118627904+Dileep9098@users.noreply.github.com>
2026-03-21 12:35:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -27,22 +27,33 @@
 
 
 
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   base: "./", // 🔥 YE ADD KARNA HI HAI
+
+//   plugins: [react(), tailwindcss()],
+
+//   css: {
+//     lightningcss: {
+//       errorRecovery: true,
+//     },
+//   },
+
+//   build: {
+//     cssMinify: "lightningcss",
+//   }
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: "./", // 🔥 YE ADD KARNA HI HAI
-
-  plugins: [react(), tailwindcss()],
-
-  css: {
-    lightningcss: {
-      errorRecovery: true,
-    },
-  },
-
-  build: {
-    cssMinify: "lightningcss",
-  }
+  plugins: [react()   , tailwindcss(),
+],
 })
